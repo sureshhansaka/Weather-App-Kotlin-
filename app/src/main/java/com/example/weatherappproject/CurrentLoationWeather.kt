0635ba9aef52852bb63cd033f8302a3138bf5830
sureshhansaka <sureshhansaka@gmail.com>
2023-11-29 14:14:31 +0530

    page commit

---
+++ app/src/main/java/com/example/weatherappproject/CurrentLoationWeather.kt
@@ -0,0 +1,11 @@
+package com.example.weatherappproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CurrentLoationWeather : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_loation_weather)
+    }
+}
---
+++ app/src/main/java/com/example/weatherappproject/DifferentLocationWeather.kt
@@ -0,0 +1,11 @@
+package com.example.weatherappproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DifferentLocationWeather : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_different_location_weather)
+    }
+}
