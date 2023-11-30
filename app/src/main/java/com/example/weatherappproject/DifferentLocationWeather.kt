88d7223dc280512b2c0a3e2f7d46c67fe81e6795
Nawoda99 <kasunnawoda152@gmail.com>
2023-11-30 19:39:42 +0530

    forcast

--- app/src/main/java/com/example/weatherappproject/DifferentLocationWeather.kt
+++ app/src/main/java/com/example/weatherappproject/DifferentLocationWeather.kt
@@ -13,6 +13,8 @@ import android.text.TextWatcher
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
@@ -27,6 +29,8 @@ import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.Locale
 
 class DifferentLocationWeather : AppCompatActivity() {
@@ -36,7 +40,7 @@ class DifferentLocationWeather : AppCompatActivity() {
     val locationClient : FusedLocationProviderClient by lazy {
         LocationServices.getFusedLocationProviderClient(this)
     }
-
+    private lateinit var forecastRecyclerView: RecyclerView
     private lateinit var lbllocation: TextView
     private lateinit var cityEdit : TextInputEditText;
     private lateinit var description: TextView
@@ -46,9 +50,18 @@ class DifferentLocationWeather : AppCompatActivity() {
     private lateinit var windSpeed: TextView
     private lateinit var imgIcon: ImageView
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_different_location_weather)
+
+
+
+
+        forecastRecyclerView = findViewById(R.id.forcast)
+        forecastRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+
 
         lbllocation = findViewById(R.id.idLbllocation)
         cityEdit = findViewById(R.id.idEdtCity)
@@ -66,7 +79,8 @@ class DifferentLocationWeather : AppCompatActivity() {
             override fun afterTextChanged(s: Editable?) {
                 val cityName = s.toString()
                 if (cityName.isNotEmpty()) {
-                    accessLocation(cityName)
+                    fetchWeatherData(cityName)
+                    fetchForecastData(cityName)
                 }
             }
 
@@ -80,37 +94,22 @@ class DifferentLocationWeather : AppCompatActivity() {
         })
 
     }
-    fun CheckPermission(){
-
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
-            accessLocation()
-        }else{
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),100)
-        }
-    }
-    @SuppressLint("MissingPermission")
-    fun accessLocation(cityName: String? = null) {
-        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 100).build()
-
-        val locationCallback = object : LocationCallback() {
-            override fun onLocationResult(p0: LocationResult) {
-                super.onLocationResult(p0)
-                p0.locations.lastOrNull()?.let { location ->
-                    fetchLocationName(location.latitude, location.longitude)
-                    fetchWeatherData(cityName ?: "", location.latitude, location.longitude)
-                }
-            }
-        }
-
-        if (cityName.isNullOrEmpty()) {
-            locationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+    fun CheckPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // No need to request location updates in this case
         } else {
-            // If cityName is provided, skip location updates and directly fetch weather data for the city
-            fetchWeatherData(cityName)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 100)
         }
     }
-    private fun fetchWeatherData(cityName: String, latitude: Double, longitude: Double) {
+
+
+
+
+    private fun fetchWeatherData(cityName: String) {
         val apiKey = "39038539455c25ce4322edfa3af922bf"
         val apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey"
 
@@ -150,32 +149,81 @@ class DifferentLocationWeather : AppCompatActivity() {
         temp.text = "$weatherTemp °C"
         windSpeed.text = "Wind Speed: $weatherWindSpeed m/s"
 
+        fetchLocationName(jsonResponse);
+
         val imageURL = "https://openweathermap.org/img/w/" + jsonResponse.getJSONArray("weather").getJSONObject(0).getString("icon")+".png"
 
         Picasso.get().load(imageURL).into(imgIcon)
     }
 
-    private fun fetchLocationName(latitude: Double, longitude: Double) {
-        val geocoder = Geocoder(this, Locale.getDefault())
 
-        try {
-            val addresses: List<Address> = geocoder.getFromLocation(latitude, longitude, 1) as List<Address>
-            if (addresses.isNotEmpty()) {
-                val address: Address = addresses[0]
-                val locationName = address.locality ?: address.subAdminArea ?: address.adminArea ?: ""
+
+    private fun fetchLocationName(jsonResponse: JSONObject) {
+        val cityName = jsonResponse.optString("name", "")
+
+        runOnUiThread {
+            lbllocation.text = if (cityName.isNotEmpty()) cityName else "Location not found"
+        }
+    }
+    private fun fetchForecastData(cityName: String) {
+
+        val apiKey = "39038539455c25ce4322edfa3af922bf"
+        val forecastApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey"
+
+        Thread {
+            try {
+                val url = URL(forecastApiUrl)
+                val urlConnection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                val inputStream = urlConnection.inputStream
+                val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+                val stringBuilder = StringBuilder()
+
+                bufferedReader.forEachLine {
+                    stringBuilder.append(it)
+                }
+
+                val forecastJsonArray = JSONObject(stringBuilder.toString()).getJSONArray("list")
+
+                val forecastList = mutableListOf<ForecastModel>()
+
+                // Get the current date in milliseconds
+                val currentDateMillis = System.currentTimeMillis()
+
+                for (i in 0 until forecastJsonArray.length()) {
+                    val forecastJson = forecastJsonArray.getJSONObject(i)
+
+                    val forecastTimestamp = forecastJson.getLong("dt") * 1000
+
+                    if (forecastTimestamp >= currentDateMillis && forecastList.size < 5) {
+                        val date = Date(forecastTimestamp)
+
+
+                        // Format the date to get the day of the week
+                        val dayFormat = SimpleDateFormat("EEEE", Locale.getDefault())
+                        val day = dayFormat.format(date)
+                        val iconUrl = "https://openweathermap.org/img/w/${forecastJson.getJSONArray("weather").getJSONObject(0).getString("icon")}.png"
+                        val temperature = "${forecastJson.getJSONObject("main").getDouble("temp")} °C"
+                        val description = forecastJson.getJSONArray("weather").getJSONObject(0).getString("description")
+
+                        val temperatureKelvin = forecastJson.getJSONObject("main").getDouble("temp")
+                        val temperatureCelsius = temperatureKelvin - 273.15
+                        val formattedTemperature = "${String.format("%.2f", temperatureCelsius)} °C"
+
+                        val forecastModel = ForecastModel(day, iconUrl, formattedTemperature, description)
+                        forecastList.add(forecastModel)
+                    }
+
+                }
 
                 runOnUiThread {
-                    lbllocation.text = "$locationName"
+                    // Create and set adapter for the forecast RecyclerView
+                    val forecastAdapter = ForecastAdapter(forecastList)
+                    forecastRecyclerView.adapter = forecastAdapter
                 }
-            } else {
-                // Handle the case where no addresses are found
-                lbllocation.text = "Location not found"
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
-        } catch (e: IOException) {
-            e.printStackTrace()
-            // Handle the exception
-            lbllocation.text = "Error fetching location name"
-        }
+        }.start()
     }
 
 
